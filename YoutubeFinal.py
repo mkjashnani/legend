3b546514d42a70be17adee36836551ee01f3dce6
mkjashnani <44252900+mkjashnani@users.noreply.github.com>
2022-02-18 23:21:04 +0530

    Update YoutubeFinal.py

--- YoutubeFinal.py
+++ YoutubeFinal.py
@@ -1,18 +1,102 @@
 import datetime
 import time
-import Youtube
-import Telegram
-import telesend
+import googleapiclient.discovery
+import requests
+import json
 lv=0
 lvt=0
+
+TOKEN = "5064671174:AAFTdDwyLtGK55K2k0wIJsioJsV5pzfcUPE"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates():
+    url = URL + "getUpdates"
+    js = get_json_from_url(url)
+    return js
+
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    #text = updates["result"][last_update]["message"]["text"]
+    text = updates['result'][-1]['message']['text']
+    #chat_id = updates["result"][2]["message"]["chat"]["id"]
+    return (text)
+
+
+def send_message(text, chat_id):
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+
+def botmsg(bot_message):
+    bot_token = '5033997656:AAHcGq2QaeKPclqzNhP2msPxYVpaJiXgvx8'
+    bot_chatID = '2025063834'#LEGEND
+    #bot_chatID1 = '1688134885'#rio
+    #bot_chatID1 = '548521105'#siva
+    #314516752
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    #send_text1='https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID1 + '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text)
+    # response1 = requests.get(send_text1)
+    return response.json()
+def Time(YTET):
+    Net=int(YTET)-time.time()
+    return Net
+def datetotimestamp(date):
+
+    element = datetime.datetime.strptime(date,"%d/%m/%Y %H:%M:%S")
+    timestamp1 = str(datetime.datetime.timestamp(element))
+    timestamp_m=str()
+
+    for i in range(0,int(len((timestamp1))-2)):
+        timestamp_m=timestamp_m+str(timestamp1[i])
+    return int(timestamp_m)
+
+def YoutubeAlgo(videoid):
+    API_NAME = 'youtube'
+    API_VERSION = 'v3'
+    service = googleapiclient.discovery.build(
+        API_NAME, API_VERSION,developerKey='AIzaSyCVIaRJ2Da61MKaWdArlO-I6hcA82ubg2w')
+    part_string = 'contentDetails,statistics,snippet'
+    video_ids = videoid
+    response = service.videos().list(
+            part=part_string,
+            id=video_ids
+        ).execute()
+    return int(response.get('items')[0]['statistics']['viewCount'])
+def Detailfetch(update):
+    lastmsg =  get_last_chat_id_and_text(update)
+    a = ''
+    b = []
+    for i in range(0, len(lastmsg)):
+        if lastmsg[i] != ',':
+            a = a + lastmsg[i]
+        elif lastmsg[i] == ',':
+            b.append(a)
+            a = ''
+    return b
+
 while True:
-    telesend.botmsg("hey")
-    update=Telegram.get_json_from_url('https://api.telegram.org/bot5033997656:AAHcGq2QaeKPclqzNhP2msPxYVpaJiXgvx8/getupdates')
-    alldel=Youtube.Detailfetch(update)
+    botmsg("hey")
+    update= get_json_from_url('https://api.telegram.org/bot5033997656:AAHcGq2QaeKPclqzNhP2msPxYVpaJiXgvx8/getupdates')
+    alldel= Detailfetch(update)
     print(alldel)
     Rview=alldel[0]
     ftime=alldel[1]
-    ftimet = Youtube.datetotimestamp(ftime)
+    ftimet =  datetotimestamp(ftime)
     video_id=alldel[2]
     a=0
     atime=0
@@ -23,34 +107,34 @@ while True:
     #
     if lv!=0 and lvt!=0:
         while True:
-            if Youtube.YoutubeAlgo(videoid=video_id)>lv:
-                nv=Youtube.YoutubeAlgo(videoid=video_id)-lv
+            if  YoutubeAlgo(videoid=video_id)>lv:
+                nv= YoutubeAlgo(videoid=video_id)-lv
                 nt=time.time()-lvt
                 nvc=nv/nt
                 rt=ftimet-time.time()
-                et=nvc*rt+Youtube.YoutubeAlgo(videoid=video_id)
+                et=nvc*rt+ YoutubeAlgo(videoid=video_id)
                 if int(et) > int(Rview):
                     print("SViews Will Meet ", "Expected Views:- ", et)
-                    telesend.botmsg(str(("Views Will Meet ", "Expected Views:- ",et)))
+                    botmsg(str(("Views Will Meet ", "Expected Views:- ",et)))
                 else:
-                    telesend.botmsg(str(("Views Will Not Meet ", "Expected Views:- ", et)))
+                    botmsg(str(("Views Will Not Meet ", "Expected Views:- ", et)))
                     print("SViews Will Not Meet ", "Expected Views:- ", et)
 
-                lv=Youtube.YoutubeAlgo(videoid=video_id)
+                lv= YoutubeAlgo(videoid=video_id)
                 lvt=time.time()
                 break
 
 
     try:
         while True:
-            if Youtube.YoutubeAlgo(videoid=video_id)>a and a==0:
-                a=int(Youtube.YoutubeAlgo(videoid=video_id))
+            if  YoutubeAlgo(videoid=video_id)>a and a==0:
+                a=int( YoutubeAlgo(videoid=video_id))
                 atime=time.time()
-            elif Youtube.YoutubeAlgo(videoid=video_id) > a and b==0 and a!=0:
-                b = int(Youtube.YoutubeAlgo(videoid=video_id))
+            elif  YoutubeAlgo(videoid=video_id) > a and b==0 and a!=0:
+                b = int( YoutubeAlgo(videoid=video_id))
                 btime = time.time()
-            elif Youtube.YoutubeAlgo(videoid=video_id) > a and Youtube.YoutubeAlgo(videoid=video_id) > b and c==0 and a!=0 and b!=0:
-                c = int(Youtube.YoutubeAlgo(videoid=video_id))
+            elif  YoutubeAlgo(videoid=video_id) > a and  YoutubeAlgo(videoid=video_id) > b and c==0 and a!=0 and b!=0:
+                c = int( YoutubeAlgo(videoid=video_id))
                 lv=c
 
                 ctime = time.time()
@@ -65,39 +149,9 @@ while True:
                 finalview=cvr*rtime+c
                 if int(finalview)>int(Rview):
                     print("Views Will Meet ","Expected Views:- ",finalview)
-                    telesend.botmsg(str(("Views Will Meet ", "Expected Views:- ", finalview)))
+                    botmsg(str(("Views Will Meet ", "Expected Views:- ", finalview)))
                 else:
-                    telesend.botmsg(str(("Views Will Not Meet ", "Expected Views:- ", finalview)))
-                    print("Views Will Not Meet ", "Expected Views:- ", finalview)
-                break
-    except:
-        pass
-
-
-                a=int(Youtube.YoutubeAlgo(videoid=video_id))
-                atime=time.time()
-            elif Youtube.YoutubeAlgo(videoid=video_id) > a and b==0 and a!=0:
-                b = int(Youtube.YoutubeAlgo(videoid=video_id))
-                btime = time.time()
-            elif Youtube.YoutubeAlgo(videoid=video_id) > a and Youtube.YoutubeAlgo(videoid=video_id) > b and c==0 and a!=0 and b!=0:
-                c = int(Youtube.YoutubeAlgo(videoid=video_id))
-                lv=c
-
-                ctime = time.time()
-                lvt=ctime
-            elif a!=0 and b!=0 and c!=0:
-                netview=int(c)-int(b)
-                nettime=int(ctime)-int(btime)
-                nettimef=nettime/60
-                cvr=netview/nettimef
-                print(cvr)
-                rtime=(ftimet-ctime)/60
-                finalview=cvr*rtime+c
-                if int(finalview)>int(Rview):
-                    print("Views Will Meet ","Expected Views:- ",finalview)
-                    telesend.botmsg(str(("Views Will Meet ", "Expected Views:- ", finalview)))
-                else:
-                    telesend.botmsg(str(("Views Will Not Meet ", "Expected Views:- ", finalview)))
+                    botmsg(str(("Views Will Not Meet ", "Expected Views:- ", finalview)))
                     print("Views Will Not Meet ", "Expected Views:- ", finalview)
                 break
     except:
